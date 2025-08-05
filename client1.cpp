b0f607409c5adef229162e77f1a4ec2e57a849d3
random_17 <167915456+Gautham-17@users.noreply.github.com>
2025-08-05 13:58:03 +0530

    Update client1.cpp

--- client1.cpp
+++ client1.cpp
@@ -1,52 +1,53 @@
 #include <iostream>
 #include <thread>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
 #include <cstring>
-
-#define PORT 8080
-#define BUFFER_SIZE 1024
-
 using namespace std;
 
-void receiveMessages(int sock) {
-    char buffer[BUFFER_SIZE];
+void receive(int sock) {
+    char buffer[1024];
     while (true) {
-        memset(buffer, 0, BUFFER_SIZE);
-        int valread = read(sock, buffer, BUFFER_SIZE);
-        if (valread <= 0) break;
-        cout << "\n[Received] " << buffer << endl;
+        memset(buffer, 0, sizeof(buffer));
+        int val = read(sock, buffer, sizeof(buffer));
+        if (val > 0) {
+            cout << "\nMessage: " << buffer << "\n> ";
+            cout.flush();
+        } else {
+            cout << "\nDisconnected from server.\n";
+            break;
+        }
     }
 }
 
 int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char buffer[BUFFER_SIZE] = {0};
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        cerr << "Socket creation failed\n";
+        return 1;
+    }
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serv_addr = {};
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(9999);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (connect(sock, (sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        cerr << "Connection failed\n";
+        return 1;
+    }
 
-    cout << "Enter your name: ";
-    string name;
-    getline(cin, name);
-    send(sock, name.c_str(), name.size(), 0);
+    thread t(receive, sock);
+    string msg;
 
-    thread(receiveMessages, sock).detach();
-
-    cout << "Send messages in format: recipient_name:message\n";
     while (true) {
-        string msg;
+        cout << "> ";
         getline(cin, msg);
-        if (msg == "/quit") break;
-        send(sock, msg.c_str(), msg.size(), 0);
+        if (msg == "exit") break;
+        send(sock, msg.c_str(), msg.length(), 0);
     }
 
     close(sock);
+    t.join();
     return 0;
 }
